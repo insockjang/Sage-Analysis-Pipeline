d43e55af1caea658431fa56a28fcae131ce84d7c
In Sock Jang <isjang@rcesdgvm04.rce.celgene.com>
2017-11-14 09:51:14 -0800

    FET update to include all stats

--- PathwayAnalysis/myPathwayAnalysis.R
+++ PathwayAnalysis/myPathwayAnalysis.R
@@ -22,7 +22,8 @@ myPathwayAnalysis <- setRefClass(Class = "myPathwayAnalysis",
                                       Mat2x2[1,2] <- length(setdiff(geneSet,testSet))
                                       Mat2x2[2,2] <- length(AllGenes) - Mat2x2[1,1]- Mat2x2[1,2]- Mat2x2[2,1]
                                       
-                                      .self$fetResult<-fisher.test(Mat2x2, alternative = "greater")                               
+                                      fisher.result <- fisher.test(Mat2x2, alternative = "greater")   
+                                      .self$fetResult <- list(pval = fisher.result$p.value,stats = fisher.result$estimate, mat = Mat2x2, geneSet = intersect(testSet,geneSet))
                                     },
                                     gseaPlot = function(reference,geneSet,...){
                                       reference1 <- sort(reference,decreasing = TRUE, index.return = TRUE)
